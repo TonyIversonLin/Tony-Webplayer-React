d6c64f37b997e3967201199e12fec8127c109461
Tony <yechao@umich.edu>
2016-10-31 12:36:48 -0400

    fetch album data from backend successfully

--- browser/react/main.js
+++ browser/react/main.js
@@ -4,54 +4,33 @@ import Footer from './Footer';
 import Sidebar from './Sidebar';
 import Album from './Album';
 
-const fakeAlbum = {
-  name: 'Abbey Road',
-  imageUrl: 'http://fillmurray.com/300/300',
-  songs: [{
-    id: 1,
-    name: 'Romeo & Juliette',
-    artists: [{name: 'Bill'}],
-    genre: 'Funk',
-    audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-  }, {
-    id: 2,
-    name: 'White Rabbit',
-    artists: [{name: 'Bill'}, {name: 'Bob'}],
-    genre: 'Fantasy',
-    audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-  }, {
-    id: 3,
-    name: 'Lucy in the Sky with Diamonds',
-    artists: [{name: 'Bob'}],
-    genre: 'Space',
-    audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-  }]
-};
-
 export default class Main extends Component {
 	
 	constructor(props) {
 		super(props);
-		this.state = Object.assign({}, fakeAlbum);
+		this.state = {
+			album: {imageUrl: '', name: '', songs: []}
+		}
+	}
+
+	componentDidMount() {
+		const toJson = response => response.json();
+		const log = console.log.bind(console);
+		const logError = console.error.bind(console);
+		fetch('api/albums/1')
+			.then(res => {
+				return res.json();
+			}).then(albumFromServer => {
+				albumFromServer.imageUrl = `/api/albums/${albumFromServer.id}/image`
+				this.setState({album: albumFromServer})
+			})
 	}
 
 	render () {
-		let songRow = this.state.songs.map((song,index) => {
-			return <tr key={index}>
-	      <td>
-	        <button className="btn btn-default btn-xs">
-	          <span className="glyphicon glyphicon-play"></span>
-	        </button>
-	      </td>
-	      <td>{song.name}</td>
-	      <td>{song.artists.map((artist)=>artist.name).join(' ')}</td>
-	      <td>{song.genre}</td>
-			</tr>
-		})
 		return (
 			<div id="main" className="container-fluid">
 				<Sidebar />
-				<Album name={this.state.name} imageUrl={this.state.imageUrl} songs={this.state.songs}/>
+				<Album name={this.state.album.name} imageUrl={this.state.album.imageUrl} songs={this.state.album.songs}/>
 				<Footer />
 			</div>
 		)
