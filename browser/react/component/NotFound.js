7744d47a7c4d141f75692248c0cfbd718b202943
Tony <yechao@umich.edu>
2016-11-10 14:49:47 -0500

    add not found page and route

---
+++ browser/react/component/NotFound.js
@@ -0,0 +1,6 @@
+'use strict';
+import React from 'react';
+
+export default () => {
+	return <h1>404 Not Found !</h1>
+}
--- browser/react/routes.js
+++ browser/react/routes.js
@@ -1,4 +1,4 @@
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, IndexRedirect} from 'react-router';
 import React, {PropTypes} from 'react';
 import AlbumsContainer from './container/AlbumsContainer';
 import AlbumContainer from './container/AlbumContainer';
@@ -7,6 +7,7 @@ import ArtistContainer from './container/ArtistContainer';
 import Main from './container/Main';
 import Albums from './component/Albums';
 import SongTable from './component/SongTable';
+import NotFound from './component/NotFound';
 
 import {fetchAlbumsFromServer, fetchSingleAlbum} from './action/albumActions';
 import {fetchArtists, fetchSingleArtist} from './action/artistActions'
@@ -19,9 +20,11 @@ export default (store) => {
 			<Route path='Albums/(:id)' component={AlbumContainer} onEnter={preLoadSingleAlbum(store)}/>
 			<Route path='Artists' component={ArtistsContainer} onEnter={preLoadAllArtists(store)}/>
 			<Route path='Artists/(:id)' component={ArtistContainer} onEnter={preLoadSingleArtist(store)}>
+				<IndexRoute component={Albums}/>
 				<Route path='albums' component={Albums}/>
 				<Route path='songs' component={SongTable}/>
 			</Route>
+			<Route path='*' component={NotFound}/>
 		</Route>
 	)
 }
